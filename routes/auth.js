04fcb38676cc6ba401e402d49646e81a1ccbc1ed
Chawin_Hotel_Booking <6570052821@student.chula.ac.th>
2023-04-26 01:41:26 +0700

    'finish_chapter8'

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const {register, login} = require('../controllers/auth');
+const {register, login, getMe} = require('../controllers/auth');
 const router = express.Router();
 
-//Path register และส่งจ่อไปให้ method register ที่ require มาจาก controllers
+const {protect} = require('../middleware/auth')
+
+//Path register และส่งต่อไปให้ method register ที่ require มาจาก controllers
 router.post('/register', register);
 router.post('/login', login);
+router.get('/me', protect, getMe);
 
 //export ให้เรียกใช้ router ของเราได้ด้วย
 module.exports = router;
--- routes/hotels.js
+++ routes/hotels.js
@@ -3,9 +3,10 @@ const {getHotels, getHotel, createHotel, updateHotel, deleteHotel} = require('..
 
 const router = express.Router();
 
-const {protect} = require('../middleware/auth');
+const {protect, authorize} = require('../middleware/auth');
 
-router.route('/').get(getHotels).post(protect, createHotel);
-router.route('/:id').get(getHotel).put(protect, updateHotel).delete(protect, deleteHotel);
+//!ใน authorize ให้ใส่ role ที่ต้องการทำ ดูได้ใน Model User
+router.route('/').get(getHotels).post(protect, authorize('admin'), createHotel);
+router.route('/:id').get(getHotel).put(protect, authorize('admin'), updateHotel).delete(protect, authorize('admin'), deleteHotel);
 
 module.exports = router;
