df670f5555df61a8da4bd1e730d969915bfbc977
Chawin_Hotel_Booking <6570052821@student.chula.ac.th>
2023-05-04 15:24:53 +0700

    'deployment2'

--- controllers/hotels.js
+++ controllers/hotels.js
@@ -126,7 +126,7 @@ exports.createHotel = async(req, res, next) => {
         }
         console.log(req.body)
         const hotel = await Hotel.create(req.body);
-        res.status(200).json({success: true, data: hotel});
+        res.status(201).json({success: true, data: hotel});
     } catch(err) {
         console.log(err)
         return res.status(500).json({success: false, message: "Cannot create hotel"})
